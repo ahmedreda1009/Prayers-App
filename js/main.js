b530f225b32fcc71285eeb023efe60c602cf34c3
ahmed reda <ahmedreda1009@gmail.com>
2023-02-12 11:27:45 +0200

    css dimentions edits

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // const userDateApi = 'http://api.aladhan.com/v1/currentDate?zone=';
 // const userTimeApi = 'http://api.aladhan.com/v1/currentTime?zone=';
 const userLocationApi = 'http://ip-api.com/json/';
-const monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+const monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 let nextPrayer = undefined;
 
@@ -10,13 +10,14 @@ const input = document.querySelector('.info input');
 async function getUserLocation() {
     const response = await axios.get(userLocationApi);
 
-    document.querySelector('.info .location .city').innerHTML = response.data.city;
+    document.querySelector('.info .location .city').innerHTML = response.data.regionName;
     document.querySelector('.info .location .country').innerHTML = response.data.countryCode;
+    console.log(response.data);
 
     return Promise.resolve(response.data);
 }
 
-async function getUserDateTime({city, country}) {
+async function getUserDateTime({ city, country }) {
     // const dateResponse = await axios.get(`${userDateApi}${timezone}`);
     // const timeResponse = await axios.get(`${userTimeApi}${timezone}`);
     // const dateArr = dateResponse.data.data.split('-');
@@ -33,11 +34,11 @@ async function getUserDateTime({city, country}) {
     document.querySelector('.info .date-time .date').innerHTML = `${day} ${monthArr[month]} ${year}`;
     document.querySelector('.info .date-time .time').innerHTML = `${hour > 12 ? `0${hour - 12}` : hour}:${minute < 10 ? `0${minute}` : minute} ${hour >= 12 ? 'PM' : 'AM'}`;
 
-    getPrayers({city, country, month, year, day, hour, minute})
+    getPrayers({ city, country, month, year, day, hour, minute })
 }
 
-async function getPrayers({city, country, month, year, day, hour, minute}) {
-    const userPrayersApi = `http://api.aladhan.com/v1/calendarByCity?city=${city}&country=${country}&method=5&month=${month < 10 ? `0${month + 1}`: month + 1}&year=${year}`;
+async function getPrayers({ city, country, month, year, day, hour, minute }) {
+    const userPrayersApi = `http://api.aladhan.com/v1/calendarByCity?city=${city}&country=${country}&method=5&month=${month < 10 ? `0${month + 1}` : month + 1}&year=${year}`;
     const response = await axios.get(userPrayersApi);
     console.log(month);
     console.log(response.data.data[day - 1].timings);
@@ -45,10 +46,10 @@ async function getPrayers({city, country, month, year, day, hour, minute}) {
 
     for (let time in timings) {
         console.log(timings[time]);
-        timings[time] = timings[time].slice(0,5);
+        timings[time] = timings[time].slice(0, 5);
 
-        const hours = parseInt(timings[time].slice(0,2));
-        const minutes = parseInt(timings[time].slice(3,5));
+        const hours = parseInt(timings[time].slice(0, 2));
+        const minutes = parseInt(timings[time].slice(3, 5));
 
         if (hours >= 12) {
             if (hours > 12) {
@@ -60,7 +61,7 @@ async function getPrayers({city, country, month, year, day, hour, minute}) {
             timings[time] = `${hours}:${minutes} AM`
         }
     }
-    
+
     const prayersTime = document.querySelectorAll('.prayers .prayer .prayer-time');
 
     prayersTime[0].innerHTML = timings.Fajr;
@@ -82,13 +83,15 @@ async function searchForCities() {
         const response = await axios.get(countryNameApi);
 
         // get country name from country code.
-        let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+        let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
         let countryName = regionNames.of(response.data.sys.country);
 
         document.querySelector('.info .location .city').innerHTML = response.data.name;
         document.querySelector('.info .location .country').innerHTML = response.data.sys.country;
 
-        getUserDateTime({city: input.value, country:countryName});
+        getUserDateTime({ city: input.value, country: countryName });
+
+        input.value = '';
 
     } catch (error) {
         console.log(error.message);
@@ -96,8 +99,8 @@ async function searchForCities() {
 }
 
 getUserLocation().then(res => {
-    
-    getUserDateTime({city: res.city, country: res.countryCode})
+
+    getUserDateTime({ city: res.city, country: res.countryCode })
 
 }).catch(e => {
     console.log(e.message);
@@ -111,4 +114,7 @@ input.addEventListener('keypress', (e) => {
     }
 });
 
+document.querySelector('.search').addEventListener('click', () => {
+    searchForCities();
+})
 
